668df251b48de8160e6a5c416c3a34c29ee1c514
ppuacgarcia <55265850+ppuacgarcia@users.noreply.github.com>
2021-09-28 23:34:35 -0600

    cambios varios

--- MetadatosPDF/src/main/java/com/url/metadatospdf/Data.java
+++ MetadatosPDF/src/main/java/com/url/metadatospdf/Data.java
@@ -1,14 +1,58 @@
 package com.url.metadatospdf;
 
 import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import javax.swing.DefaultListModel;
 
 public class Data {
+    private DefaultListModel modelo = new DefaultListModel();
+    private String path;
+    private Node s;
     public Data(){
+        s=null;
     }
-    public void Files(File Folder){
+    public DefaultListModel Files1(File Folder){
+        modelo = new DefaultListModel();
+        return Files2(Folder);
+    }
+    private DefaultListModel Files2(File Folder){
         for(File file:Folder.listFiles()){
-            if(!file.isDirectory())System.out.println("" + file.getName());
-            else Files(file);
+            
+            if(!file.isDirectory()){
+                Node n=new Node();
+                Node prev=prev(s,null);
+                n.setFile(file);
+                if(s==null){
+                    n.setNext(null);
+                    s=n;
+                }else{
+                   prev.setNext(n);
+                   n.setNext(null);
+                }
+                
+                modelo.addElement(file.getName());            
+            }
+            else Files1(file);
         }
+        return modelo;
+    }
+    private Node prev(Node aux,Node value){
+        while(aux.getNext()!=value){
+           aux=aux.getNext();
+        }
+        return aux;
+    }
+    public String values(String name) throws IOException{
+       String r="";
+       Node aux=s;
+       while(aux.getFile().getName()!=name && aux!=null){
+           aux=aux.getNext();
+       }
+       File f=aux.getFile();
+       BasicFileAttributes at= Files.readAttributes(f.toPath(), BasicFileAttributes.class);
+       r="Name: "+f.getName()+"\nPath: "+f.toPath()+"\nSize: "+at.size()+"\nCreationTime: "+at.creationTime();
+       return r;
     }
 }
