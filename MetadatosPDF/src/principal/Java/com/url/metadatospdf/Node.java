97ed5b5e45fd3df97e56d0667b801085d2b9c7e8
Josue Maldonado Lepe <61367923+Jos-mlp@users.noreply.github.com>
2021-11-15 20:12:20 -0600

    Update and rename MetadatosPDF/src/main/java/com/url/metadatospdf/Node.java to MetadatosPDF/src/principal/Java/com/url/metadatospdf/Node.java

--- MetadatosPDF/src/principal/Java/com/url/metadatospdf/Node.java
+++ MetadatosPDF/src/principal/Java/com/url/metadatospdf/Node.java
@@ -5,9 +5,11 @@ import java.io.File;
 public class Node {
    File file;
    Node next;
+   long pos;
     public Node(){
         this.file=null;
         this.next=null;
+        this.pos=(long)0;
    } 
    public void setFile(File file){
        this.file=file;
@@ -15,6 +17,12 @@ public class Node {
    public File getFile(){
        return this.file;
    }
+    public long getPos() {
+        return this.pos;
+    }
+    public void setPos(long pos){
+        this.pos=pos;
+    }
    public void setNext(Node next){
        this.next=next;
    }
